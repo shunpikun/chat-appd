a43947519b9f644ebef719940ba6ea9fe86755de
shunpikun <moirir0906@gmail.com>
2022-03-06 20:45:05 +0900

    修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -45,7 +45,10 @@ RSpec.describe User, type: :model do
       end
 
       it 'passwordとpassword_confirmationが不一致では登録できない' do
-        
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
     end
   end
